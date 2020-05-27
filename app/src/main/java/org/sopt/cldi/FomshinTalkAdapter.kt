aa4ca4e2e87a5b52d24917d8120c39db599b53d6
jinsu4755 <parkjinsu4755@gmail.com>
2020-05-28 02:41:49 +0900

    [CREATE] ( close #1 ) 곰신톡 Adapter, ViewHolder 추가

---
+++ app/src/main/java/org/sopt/cldi/FomshinTalkAdapter.kt
@@ -0,0 +1,21 @@
+package org.sopt.cldi
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class GomshinTalkAdapter (val datas:MutableList<Any>, val context: Context) : RecyclerView.Adapter<GomshinTalkViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GomshinTalkViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.gomshin_talk_item,parent,false)
+        return GomshinTalkViewHolder(view)
+    }
+
+    override fun getItemCount() = datas.size
+
+    override fun onBindViewHolder(holder: GomshinTalkViewHolder, position: Int) {
+        holder.bind(datas[position])
+    }
+
+}
---
+++ app/src/main/java/org/sopt/cldi/GomshinTalkViewHolder.kt
@@ -0,0 +1,11 @@
+package org.sopt.cldi
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class GomshinTalkViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(customData: Any) {
+
+    }
+}
