fa3623414da312d5cba13fbfb7222a3cf8683d6e
june <brainyjun@gmail.com>
2020-05-29 01:25:28 +0900

    [사다리 2번] 타이틀, 검색, 태그, 베스트 군샵

---
+++ app/src/main/java/org/sopt/cldi/BestGunShopData.kt
@@ -0,0 +1,8 @@
+package org.sopt.cldi
+
+data class BestGunShopData(
+    val rank: Int,
+    val image: Int,
+    val shopName: String,
+    val description: String
+)
--- app/src/main/java/org/sopt/cldi/MainActivity.kt
+++ app/src/main/java/org/sopt/cldi/MainActivity.kt
@@ -1,12 +1,45 @@
 package org.sopt.cldi
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initRequestFocus()
+        initPopularTags()
+        initBestGunShopRecyclerView()
+    }
+
+    private fun initRequestFocus() {
+        edtSearch.requestFocus()
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY)
+    }
+
+    private fun initPopularTags() {
+        listOf("가나다", "라마바", "사아자", "일이삼")
+            .asSequence()
+            .map { "#$it" }
+            .toList()
+            .forEach {
+                val tag = layoutInflater.inflate(R.layout.item_popular_tag, tagContainer, false) as TextView
+                tag.text = it
+                tagContainer.addView(tag)
+            }
+    }
+
+    private fun initBestGunShopRecyclerView() {
+        rvBestGunShop.apply {
+            layoutManager = LinearLayoutManager(applicationContext)
+            adapter = BestGunShopRecyclerView().apply { notifyDataSetChanged() }
+        }
     }
 }
