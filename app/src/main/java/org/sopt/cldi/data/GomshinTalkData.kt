096a4ed75d2afe62f4a4383e731b015f64e40c0e
june <brainyjun@gmail.com>
2020-05-30 14:34:07 +0900

    data class 수정

--- app/src/main/java/org/sopt/cldi/data/GomshinTalkData.kt
+++ app/src/main/java/org/sopt/cldi/data/GomshinTalkData.kt
@@ -3,19 +3,24 @@ package org.sopt.cldi.data
 import com.google.gson.annotations.SerializedName
 
 data class GomshinTalkData(
+    val postIdx: Int,
     val title: String,
     val category: String,
     val commentNum: Int,
     val likeNum: Int,
     val createdAt: String,
-    val bestNum: Int,
-    val user:User
-)
-
-data class User(
-    val nickname:String,
+    val userIdx: Int,
+    val nickname: String,
     val level: Int,
     val military: String,
     @SerializedName("class")
-    val military_class: String
+    val military_class: String,
+    val bestNum: Int
+)
+
+data class BaseResponse<T> (
+    val status: Int,
+    val success: Boolean,
+    val message: String?,
+    val data: T?
 )
